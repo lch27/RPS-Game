96bac19da04a2e2f34bf3848403a6d73db367a9f
lch27 <mag.anderson96@gmail.com>
2024-01-03 22:02:30 +0900

    added more script

--- Main.py
+++ Main.py
@@ -5,3 +5,14 @@ print('Rock, Paper, Scissors')
 wins = 0
 losses = 0
 ties = 0
+
+while True: # The main game loop.
+    print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
+    while True: #The player input loop.
+        print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
+        playerMove = input()
+        if playerMove == 'q':
+            sys.exit()
+        if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
+            break
+        print('Type one of r, p, s,')
